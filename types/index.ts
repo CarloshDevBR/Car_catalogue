b4a2af048adf9db132550b896e616f6bababd585
CarlosDevBR <carloshdevbr@gmail.com>
2023-08-13 16:48:06 -0300

    interface custombuttonprops alterado

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,12 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
+    isDisabled?: boolean;
     btnType?: "button" | "submit";
-    containerStyles?: string; 
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -13,16 +16,16 @@ export interface SearchManuFacturerProps {
 }
 
 export interface CarProps {
-    city_mpg: number;
-    class: string;
-    combination_mpg: number;
-    cylinders: number;
-    displacement: number;
-    drive: string;
-    fuel_type: string;
-    highway_mpg: number;
-    make: string;
-    model: string;
-    transmission: string;
-    year: number;
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
