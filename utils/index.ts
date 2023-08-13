34911e323956093c0121bd7097dd8ba90fb52a2a
CarlosDevBR <carloshdevbr@gmail.com>
2023-08-13 16:49:06 -0300

    chave alterada

--- utils/index.ts
+++ utils/index.ts
@@ -1,17 +1,18 @@
 export async function fetchCars() {
     const headers: HeadersInit = {
-      "X-RapidAPI-Key": "18WJMUA9DGp+2QxoQdIRYQ==qSFHgfx9P70g4jk8",
-      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-    };
-  
+      'X-RapidAPI-Key': '01757fce83msh28e2638f9be1ff7p101435jsn90f1354f0b4f',
+      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
     const response = await fetch(
-      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carolla`,
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`,
       {
         headers: headers,
       }
     );
+  
 
     const result = await response.json();
-  
+
     return result;
-}
+} 
