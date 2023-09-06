8b4b3577c11b7c7810a89e4606257e1621c44a80
CarlosDevBR <carloshdevbr@gmail.com>
2023-09-06 16:06:32 -0300

    chore: botao testado

--- components/CustomButton/CustomButton.test.tsx
+++ components/CustomButton/CustomButton.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import { CustomButton } from './';
@@ -5,6 +7,14 @@ import { CustomButton } from './';
 describe('Test group CustomButton', () => {
   it('Should correctly render', () => {
     render(<CustomButton />);
+  });
+
+  it('Should render button with title', () => {
+    const { getByText } = render(<CustomButton title="Click-me!" />);
+
+    const button = getByText('Click-me!');
+
+    expect(button).toBeInTheDocument('Click-me!');
   });
 
   it('Should clicked button correctly', () => {
@@ -39,5 +49,36 @@ describe('Test group CustomButton', () => {
     const button = screen.getByRole('button');
 
     expect(button).toHaveAttribute('type', 'button');
+  });
+
+  it('Should render button with textStyles', () => {
+    const { getByRole } = render(<CustomButton textStyles="text-sm" />);
+
+    const button = getByRole('button');
+
+    expect(button).toHaveStyle({
+      fontSize: 'text-sm',
+    });
+  });
+
+  it('Should render button with containerStyles', () => {
+    const { getByRole } = render(<CustomButton containerStyles="bg-white" />);
+
+    const button = getByRole('button');
+
+    expect(button).toHaveStyle({
+      backgroundColor: 'bg-white',
+    });
+  });
+
+  it('Should render currently image icon', () => {
+    const rightIconSrc = '/caminho/para/sua/imagem.png';
+
+    const { getByRole } = render(<CustomButton rightIcon={rightIconSrc} />);
+
+    const img = getByRole('img');
+
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', '/_next/image?url=%2Fcaminho%2Fpara%2Fsua%2Fimagem.png&w=3840&q=75');
   });
 });
