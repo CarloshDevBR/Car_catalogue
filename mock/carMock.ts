55a38e595941907eedb852a15ed9b00bef4ef1d6
CarlosDevBR <carloshdevbr@gmail.com>
2023-09-13 22:13:01 -0300

    test: mock de cardetails criado

---
+++ mock/carMock.ts
@@ -0,0 +1,16 @@
+import { CarProps } from '@/types';
+
+export const mockCarDetails: CarProps = {
+  city_mpg: 25,
+  class: 'Sedan',
+  combination_mpg: 30,
+  cylinders: 4,
+  displacement: 2000,
+  drive: 'Front-wheel drive',
+  fuel_type: 'Gasoline',
+  highway_mpg: 35,
+  make: 'Toyota',
+  model: 'Camry',
+  transmission: 'Automatic',
+  year: 2022,
+};
