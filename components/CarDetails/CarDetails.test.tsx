f09057e68ef05a47e276d544522ce2866152c6b8
CarlosDevBR <carloshdevbr@gmail.com>
2023-09-17 09:28:06 -0300

    test: componente cardetails testado

--- components/CarDetails/CarDetails.test.tsx
+++ components/CarDetails/CarDetails.test.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { fireEvent, render } from '@testing-library/react';
-
+import { fireEvent, render, act } from '@testing-library/react';
 import { CarDetails } from './';
-
 import { mockCarDetails } from '@/mock/carMock';
 
-describe('Test group CarDetails', () => {
+describe('CarDetails Component', () => {
   beforeAll(() => {
     window.ResizeObserver = jest.fn(() => ({
       observe: jest.fn(),
@@ -19,42 +17,48 @@ describe('Test group CarDetails', () => {
     window.ResizeObserver.mockRestore();
   });
 
-  it('Should correctly render', () => {
+  it('renders correctly', () => {
     render(<CarDetails isOpen={true} car={mockCarDetails} closeModal={() => {}} />);
   });
 
-  it('Should open when click card', () => {
+  it('opens when clicked', () => {
     const { getByRole } = render(<CarDetails isOpen={true} car={mockCarDetails} closeModal={() => {}} />);
 
     const openButton = getByRole('button');
 
+    act(() => {
+      fireEvent.click(openButton);
+    });
+
     expect(openButton).toBeDefined();
   });
 
-  it('Should close the modal when the close button is clicked', () => {
+  it('closes when close button is clicked', () => {
     const { getByRole, queryByText } = render(<CarDetails isOpen={true} car={mockCarDetails} closeModal={() => {}} />);
 
     const closeButton = getByRole('button');
 
-    fireEvent.click(closeButton);
+    act(() => {
+      fireEvent.click(closeButton);
+    });
 
     expect(queryByText('car model')).toBeNull();
   });
 
-  it('Should render details car', () => {
+  it('renders car details', () => {
     const { getByText } = render(<CarDetails isOpen={true} car={mockCarDetails} closeModal={() => {}} />);
 
-    expect(getByText('25')).toBeInTheDocument();
-    expect(getByText('Sedan')).toBeInTheDocument();
-    expect(getByText('30')).toBeInTheDocument();
-    expect(getByText('4')).toBeInTheDocument();
-    expect(getByText('2000')).toBeInTheDocument();
-    expect(getByText('Front-wheel drive')).toBeInTheDocument();
-    expect(getByText('Gasoline')).toBeInTheDocument();
-    expect(getByText('35')).toBeInTheDocument();
-    expect(getByText('Toyota')).toBeInTheDocument();
-    expect(getByText('Camry')).toBeInTheDocument();
-    expect(getByText('Automatic')).toBeInTheDocument();
-    expect(getByText('2022')).toBeInTheDocument();
+    expect(getByText(mockCarDetails.city_mpg.toString())).toBeInTheDocument();
+    expect(getByText(mockCarDetails.class)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.combination_mpg.toString())).toBeInTheDocument();
+    expect(getByText(mockCarDetails.cylinders.toString())).toBeInTheDocument();
+    expect(getByText(mockCarDetails.displacement.toString())).toBeInTheDocument();
+    expect(getByText(mockCarDetails.drive)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.fuel_type)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.highway_mpg.toString())).toBeInTheDocument();
+    expect(getByText(mockCarDetails.make)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.model)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.transmission)).toBeInTheDocument();
+    expect(getByText(mockCarDetails.year.toString())).toBeInTheDocument();
   });
 });
