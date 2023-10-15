28837fa9fbf0ba5b8aa4364e65d716a020471b37
CarlosDevBR <carloshdevbr@gmail.com>
2023-10-15 17:30:58 -0300

    feat: filtro criado

--- src/components/CustomFilter/index.tsx
+++ src/components/CustomFilter/index.tsx
@@ -1,62 +1,58 @@
-'use client'
+'use client';
 
-import { Fragment, useState } from "react";
+import { Fragment, useState } from 'react';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
-import { updateSearchParams } from "../../functions/updateSearchParams";
+import { updateSearchParams } from '../../functions/updateSearchParams';
 
-import { CustomFilterProps } from "../../types";
+import { CustomFilterProps } from '../../types';
 
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, Transition } from '@headlessui/react';
 
-import Image from "next/image";
-
+import Image from 'next/image';
 
 export const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const [selected, setSelected] = useState(options[0])
+  const [selected, setSelected] = useState(options[0]);
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const handleUpdateParams = (e: { title: string, value: string}) => {
-    const newPathname = updateSearchParams(title, e.value.toLocaleLowerCase())
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathname = updateSearchParams(title, e.value.toLocaleLowerCase());
 
-    router.push(newPathname)
-  }
+    router.push(newPathname);
+  };
 
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => {
-          setSelected(e)
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
 
-          handleUpdateParams(e)
-        }}>
+          handleUpdateParams(e);
+        }}
+      >
         <div className="relative w-fit z-10">
-          <Listbox.Button className='custom-filter__btn'>
-            <span className="block truncate">
-              {selected.title}
-            </span>
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
 
-            <Image src='/chevron-up-down.svg' alt="up-down" width={20} height={20} className="ml-4 object-contain" />
+            <Image src="/chevron-up-down.svg" alt="up-down" width={20} height={20} className="ml-4 object-contain" />
           </Listbox.Button>
 
-          <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom="opacity-100" leaveTo="opacity-0">
-            <Listbox.Options className='custom-filter__options'>
+          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+            <Listbox.Options className="custom-filter__options">
               {options.map((option) => (
                 <Listbox.Option
                   key={option.title}
                   className={({ active }) =>
-                    `relative cursor-default select-none py-2 px-4 ${
-                      active ? "bg-primary-blue text-white" : "text-gray-900"
-                    }`
+                    `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`
                   }
                   value={option}
                 >
                   {({ selected }) => (
                     <>
-                      <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`} >
-                        {option.title}
-                      </span>
+                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{option.title}</span>
                     </>
                   )}
                 </Listbox.Option>
@@ -66,5 +62,5 @@ export const CustomFilter = ({ title, options }: CustomFilterProps) => {
         </div>
       </Listbox>
     </div>
-  )
+  );
 };
