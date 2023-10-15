032e2cf6954b2c03c0a752adae00f69d23adf957
CarlosDevBR <carloshdevbr@gmail.com>
2023-10-15 17:31:20 -0300

    chore: arquivo formatado

--- src/functions/updateSearchParams.ts
+++ src/functions/updateSearchParams.ts
@@ -1,9 +1,9 @@
 export const updateSearchParams = (type: string, value: string) => {
-    const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = new URLSearchParams(window.location.search);
 
-    searchParams.set(type, value);
+  searchParams.set(type, value);
 
-    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
-    return newPathname
-}
+  return newPathname;
+};
