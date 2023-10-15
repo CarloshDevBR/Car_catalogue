46cc13f3bcee2ea53c24e1d2bc2727a467cf9f5b
CarlosDevBR <carloshdevbr@gmail.com>
2023-10-15 13:13:08 -0300

    feat: funcao de criacao de parametros adicionada

--- src/components/CustomFilter/index.tsx
+++ src/components/CustomFilter/index.tsx
@@ -4,22 +4,33 @@ import { Fragment, useState } from "react";
 
 import { useRouter } from "next/navigation";
 
-import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/src/functions/updateSearchParams";
+
+import { CustomFilterProps } from "../../types";
 
 import { Listbox, Transition } from "@headlessui/react";
 
 import Image from "next/image";
+
 
 export const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0])
 
   const router = useRouter()
 
-  const handleUpdateParams = () => {}
+  const handleUpdateParams = (e: { title: string, value: string}) => {
+    const newPathname = updateSearchParams(title, e.value.toLocaleLowerCase())
+
+    router.push(newPathname)
+  }
 
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox value={selected} onChange={(e) => {
+          setSelected(e)
+
+          handleUpdateParams(e)
+        }}>
         <div className="relative w-fit z-10">
           <Listbox.Button className='custom-filter__btn'>
             <span className="block truncate">
